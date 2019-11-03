a7e0ba2aa755490788c41fe517eb55d217d42586
CORSAIRONE\sylva <sguerin@guess-engineering.com>
2019-11-03 21:48:17 +0100

    env fixed

--- scripts/client/lib/environment.py
+++ scripts/client/lib/environment.py
@@ -24,5 +24,10 @@ class Environment:
         # get screenshot as a [192,256,3] array with the last dimension representing RGB
         # this is the actual resolution of the game in the emulator
         # no need to do any extra processing
-        screenshot = np.array(ImageGrab.grabclipboard()) 
+        img = ImageGrab.grabclipboard()
+        # just a security because from time to time, img may be None...
+        while img is None:
+            img = ImageGrab.grabclipboard()
+        
+        screenshot = np.array(img) 
         return(reward, screenshot, done)
